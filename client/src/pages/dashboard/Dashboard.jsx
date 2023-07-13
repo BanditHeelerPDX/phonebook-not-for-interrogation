48c114943c007f46a7941c7d6b465d19d0ba56b9
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-07-13 11:08:41 -0700

    dashboard page updated

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -3,8 +3,22 @@ import Header from "../../components/header/Header";
 import SideLeft from "../../components/sideLeft/SideLeft";
 import SideRight from "../../components/sideRight/SideRight";
 import Timeline from "../../components/timeline/Timeline";
+import axios from "axios";
+import { useParams } from "react-router";
+import { useEffect, useState } from "react";
 
 export default function Dashboard() {
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const [user, setUser] = useState({});
+  const username = useParams().username;
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await axios.get(`/users?username=${username}`);
+      setUser(res.data);
+    };
+    fetchUser();
+  }, [username]);
   return (
     <>
       <Header />
@@ -13,17 +27,21 @@ export default function Dashboard() {
         <div className="dashboardRight">
           <div className="dashboardRightAbove">
             <div className="dashboardCloak">
-            <img src={user.coverImage} alt="" className="dashboardCoverImage" />
-            <img src={user.profileImage} alt="" className="dashboardProfileImage" />
+            <img src={user.coverPicture
+                    ? PF + user.coverPicture
+                    : PF + "person/noCover.png"} alt="" className="dashboardCoverImage" />
+            <img src={user.profilePicture
+                    ? PF + user.profilePicture
+                    : PF + "person/noAvatar.png"} alt="" className="dashboardProfileImage" />
             </div>
             <div className="dashboardDeets">
-              <h3 className="dashboardDeetsName">{user.userName}</h3>
-              <span className="dashboardDeetsDeets"></span>
+              <h3 className="dashboardDeetsName">{user.username}</h3>
+              <span className="dashboardDeetsDeets">{user.desc}</span>
             </div>
           </div>
           <div className="dashboardRightBelow">
-            <SideLeft dashboard/>
-            <Timeline />
+            <SideLeft user={user} />
+            <Timeline username={username} />
           </div>
         </div>
       </div>
