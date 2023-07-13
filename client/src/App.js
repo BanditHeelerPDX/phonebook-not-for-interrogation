efbc35bedd179bdfd94717038f06a6ea148814b7
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-07-13 10:43:39 -0700

    index and app.js updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,34 @@
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Dashboard from "./pages/dashboard/Dashboard";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
+import { useContext } from "react";
 
 function App() {
-  return <Home />;
+  const { user } = useContext(AuthContext);
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {user ? <Home /> : <Register />}
+        </Route>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        <Route path="/register">
+          {user ? <Redirect to="/" /> : <Register />}
+        </Route>
+        <Route path="/dashboard/:username">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
