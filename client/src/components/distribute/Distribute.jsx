fd226bd790df4d911d62f57db28206dfb9e1ce53
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-07-13 11:57:53 -0700

    distribute component updated

--- client/src/components/distribute/Distribute.jsx
+++ client/src/components/distribute/Distribute.jsx
@@ -1,23 +1,85 @@
 import "./distribute.css";
-import { BsFolderPlus } from "react-icons/bs";
+import { BsFolderPlus, BsFillSignStopFill } from "react-icons/bs";
 import { RiEmojiStickerLine } from "react-icons/ri";
 import { FaPeopleArrows, FaCanadianMapleLeaf } from "react-icons/fa";
+import axios from "axios";
+import { AuthContext } from "../../context/AuthContext";
+import { useContext, useRef, useState } from "react";
 
 export default function Distribute() {
+  const { user } = useContext(AuthContext);
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const desc = useRef();
+  const [file, setFile] = useState(null);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const newPost = {
+      userId: user._id,
+      desc: desc.current.value,
+    };
+    if (file) {
+      const data = new FormData();
+      const fileName = Date.now() + file.name;
+      data.append("name", fileName);
+      data.append("file", file);
+      newPost.img = fileName;
+      console.log(newPost);
+      try {
+        await axios.post("/upload", data);
+      } catch (err) {}
+    }
+    try {
+      await axios.post("/posts", newPost);
+      window.location.reload();
+    } catch (err) {}
+  };
   return (
     <div className="distributeShroud">
       <div className="distributeCloak">
         <div className="distributeAbove">
-          <img src="/assets/viking.jpg" alt="" className="distributeProfImg" />
-          <input placeholder="Shout it out, bruv!" className="distributeText" />
+          <img
+            src={
+              user.profileImage
+                ? PF + user.profileImage
+                : PF + "person/noAvatar.png"
+            }
+            alt=""
+            className="distributeProfImg"
+          />
+          <input
+            ref="desc"
+            placeholder={"Shout it out " + user.username + "!"}
+            className="distributeText"
+          />
         </div>
         <hr className="distributeHr" />
+        {file && (
+          <div className="distributeImgContainer">
+            <img
+              className="distributeImg"
+              src={URL.createObjectURL(file)}
+              alt=""
+            />
+            <BsFillSignStopFill
+              className="distributeCancel"
+              onClick={() => setFile(null)}
+            />
+          </div>
+        )}
         <div className="distributeBelow">
           <div className="distributeSelections">
-            <div className="distributeSelection">
+            <label htmlFor="file" className="distributeSelection">
               <BsFolderPlus className="distributeSelectionIcon" />
               <span className="distributeSelectionText">Proof?</span>
-            </div>
+              <input
+                style={{ display: "none" }}
+                type="file"
+                id="file"
+                accept=".png,.jpeg,.jpg"
+                onChange={(e) => setFile(e.target.files[0])}
+              />
+            </label>
             <div className="distributeSelection">
               <RiEmojiStickerLine className="distributeSelectionIcon" />
               <span className="distributeSelectionText">How ya feelin?</span>
--- client/src/components/friends/Friends.jsx
+++ client/src/components/friends/Friends.jsx
@@ -1,9 +1,10 @@
 import "./friends.css"
 
 export default function Friends({ user }) {
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   return (
     <li className="sideRightFriend">
-                   <img src={user.profilePicture} alt="" className="sideRightFriendImg" />
+                   <img src={PF+user.profileImage} alt="" className="sideRightFriendImg" />
                      <span className="sideRightFriendName">{user.userName}</span>
                 </li>
   )
