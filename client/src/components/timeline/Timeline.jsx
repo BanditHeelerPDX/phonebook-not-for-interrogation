44643614fa7707619862dd834803f53b973288e6
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-07-13 11:18:46 -0700

    fixed directory error and updated timeline component

--- client/src/components/timeline/Timeline.jsx
+++ client/src/components/timeline/Timeline.jsx
@@ -1,14 +1,33 @@
 import "./timeline.css";
 import Distribute from "../distribute/Distribute";
 import Contribute from "../contribute/Contribute";
+import { AuthContext } from "../../context/AuthContext";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
 
-export default function Timeline() {
+export default function Timeline({ username }) {
+  const [posts, setPosts] = useState([]);
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = username
+        ? await axios.get("/posts/profile/" + username)
+        : await axios.get("posts/timeline/" + user._id);
+      setPosts(
+        res.data.sort((p1, p2) => {
+          return new Date(p2.createdAt) - new Date(p1.createdAt);
+        })
+      );
+    };
+    fetchPosts();
+  }, [username, user._id]);
   return (
     <div className="timeShroud">
       <div className="timeCloak">
-        <Distribute />
-        {Posts.map((p) => (
-          <Contribute key={p.id} post={p} />
+      {(!username || username === user.username) && <Distribute />}
+        {posts.map((p) => (
+          <Contribute key={p._id} post={p} />
         ))}
       </div>
     </div>
